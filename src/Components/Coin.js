230ac0db5beb08cfc87a413f62e1e14157e0fe3a
John Youn <johnyoun28@gmail.com>
2021-10-12 21:02:50 -0700

    data showing and styling

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
 const Coin = (props) => {
+
+
+
+
     return (
         <div className='coin-container'>
-            <h2>{props.coin.name}</h2> 
+            <div className='coin-row'>
+                <img src={props.coin.image} alt='crypto'/>
+                <h1>{props.coin.name}</h1> 
+
+                <div className= 'coin'>
+                <p className='coin-symbol'>{props.coin.symbol}</p>
+            <p className='coin-price'>${props.coin.current_price}</p>
+            <p className='coin-volume'>${props.coin.total_volume.toLocaleString()}</p>
+            <p className='coin-cap'>${props.coin.market_cap.toLocaleString()}</p>
+                </div>
 
 
-            <h2>{props.coin.symbol}</h2>
-            <h2>{props.coin.current_price}</h2>
-            <h2>{props.coin.market_cap}</h2>
 
-    
 
-            
+
+
+            </div>
         </div>
     )
 }
